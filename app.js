9d9c81eba9e3110f84f5fb4beddddc2a417c3274
hashir4560 <haashirraja@gmail.com>
2023-10-06 16:22:07 +0500

    product and shop done

---
+++ app.js
@@ -0,0 +1,24 @@
+/* Core Modules
+https==> Launch a server and sends requests
+http ==> Launch a SSL server
+fs 
+path
+os
+*/
+const path = require("path");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
