285993f8e6ab8a7ec84d9eb32d3888753c296fdd
hashir4560 <haashirraja@gmail.com>
2023-10-06 19:21:46 +0500

    Added Styles and made changes

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //admin/add-product =>  GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 //admin/add-product =>  POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
